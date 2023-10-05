0debfc2a8cf8aaf6079abcccccde2b98ad5a14d8
Yan Nascimento <yanzebedeu@hotmail.com>
2023-10-05 06:13:16 +0200

    Aggiungere i file di progetto.

---
+++ elaborazione-daticsv-cahino/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elaborazione_daticsv_cahino
+{
+    internal class funzioni
+    {
+        public int campi(string filename, char delim)
+        {
+            StreamReader sr = new StreamReader(filename);
+            string s = sr.ReadLine();
+            sr.Close();
+            return s.Split(delim).Length;
+        }
+    }
+}
---
+++ elaborazione-daticsv-cahino/Form1.cs
@@ -0,0 +1,20 @@
+namespace elaborazione_daticsv_cahino
+{
+    public partial class Form1 : Form
+    {
+        funzioni f;
+        string filename;
+        string filename1;
+        public Form1()
+        {
+            string filename = @"Cahino1.csv";
+            string filename1 = @"Cahino.csv";
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
